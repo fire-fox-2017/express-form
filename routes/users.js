91cab85988303b8be020329fe5076c4ea56520ec
edimdendidiwangga <edimdendy@gmail.com>
2017-04-15 15:28:38 +0700

    expres form edim

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('hello '+ req.query.say);
+  //res.send('hello '+ req.query.say);
+  res.render('users', {firstname: req.query.firstname || "John", lastname: req.query.lastname || "Doe"});
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('users', {firstname: req.query.firstname || "John", lastname: req.query.lastname || "Doe"});
 });
 
 module.exports = router;
