ff83a227825d2eabf9f638a624078f8f6c56a129
parelhutahaean <parel.hutahaean@gmail.com>
2017-04-18 15:37:05 +0700

    Parel Hutahaean Finished

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users',{out:""})
+  // res.send('respond with a resource');
+});
+
+router.get('/input', function(req, res, next){
+  let output = `Result : ${(req.body.name).toLowerCase()}`;
+  res.render('users', {out: output});
+});
+
+router.post('/input', function(req, res, next){
+  let output = `Result : ${(req.body.name).toLowerCase()}`;
+  res.render('users', {out: output});
 });
 
 module.exports = router;
